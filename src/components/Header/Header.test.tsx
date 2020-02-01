3eb17b390d62e3f05243d4050353a853c84f3098
emreylmz <emreylmz08@gmail.com>
2020-02-01 10:50:22 +0300

    Update headers unit tests

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -6,16 +6,16 @@ import Header from './Header'
 describe('Header', () => {
   beforeEach(() => {
   }, 1000);
-  it('renders correctly', () => {
+  it('should be renders correctly', () => {
     const component = renderer.create(<Header/>)
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot();
   })
 
-  it('has logo', () => {
+  it('should have title', () => {
     const root = renderer.create(<Header/>).root
-    const img = root.findByType('img')
+    const img = root.findByType('h1')
     expect(img).toBeDefined()
-    expect(img.props.className.includes('logo')).toBeTruthy()
+    expect(img.props.className.includes('title')).toBeTruthy()
   })
 })
